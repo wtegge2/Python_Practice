5086f5ed51f535febf2b1a0a976d1da6bbcdc914
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-04 18:06:35 -0500

    init

---
+++ list_slices_practice.py
@@ -0,0 +1,12 @@
+
+star_wars_characters = ['luke skywalker', 'darth vader', 'han solo', 'chewbacca', 'jabba', 'boba fett']
+
+print("The first three items in the list are: ")
+print(star_wars_characters[:3])
+
+print("The middle items in the list are: ")
+print(star_wars_characters[1:5])
+
+print("The last three items of the list are: ")
+print(star_wars_characters[3:])
+
