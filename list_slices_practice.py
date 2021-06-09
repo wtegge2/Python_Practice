a624b4965ccba1f061b9a442be68edd27c29a6ad
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-09 12:37:06 -0500

    added more stuff

--- list_slices_practice.py
+++ list_slices_practice.py
@@ -10,3 +10,9 @@ print(star_wars_characters[1:5])
 print("The last three items of the list are: ")
 print(star_wars_characters[3:])
 
+
+my_food = ['Pizza', 'Taco', 'Apple', 'Cheeseburger']
+friends_food = my_food[:]
+
+my_food.append('Chips')
+
