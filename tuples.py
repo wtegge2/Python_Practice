251beb3f3ad5ae7f10260e4a89083e4deede5ec8
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-09 13:26:28 -0500

    did more practice with tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+buffet_foods = ('soup', 'salad', 'hotdogs', 'burgers', 'fish')
+
+for food in buffet_foods:
+    print(food)
+
+buffet_foods = ('soup', 'salad', 'fruit', 'ice cream', 'fish')
+
+for new_food in buffet_foods:
+    print(new_food)
+
