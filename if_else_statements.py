54dbc6e8d4c394f17b03d0d20082f11b1872a30e
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 13:12:14 -0500

    new file

---
+++ if_else_statements.py
@@ -0,0 +1,20 @@
+
+# If-else statements practice
+
+
+magic_number = 32
+
+if magic_number < 0:
+    print("The magic number is negative!")
+
+else:
+    print("The magic number is positive!")
+
+
+# Practice with boolean logic
+if magic_number > 0 and magic_number < 50:
+    print("The magic number is less than 50!")
+
+if magic_number > 0 and magic_number > 100:
+    print("The magic number is greater than 100!")
+
