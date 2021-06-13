bb35d15f94e8f4bbb286bfb916797a2c9ccfcc3c
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 15:14:54 -0500

    init

---
+++ lists_and_if_and_for.py
@@ -0,0 +1,11 @@
+
+usernames = ['weeWilly123', 'Bryguy7', 'admin', 'Noobmaster69', 'SisterSky400']
+
+for name in usernames:
+    if name == 'admin':
+        print("Hello Admin! What would you like to do today? You have many options because you are the admin.")
+
+    else:
+        print(f "Hello {name}! What would you like to do?")
+
+        
