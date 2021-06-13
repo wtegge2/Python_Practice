d26266cbb5b33abf9eb069f9750f5954fbf45af1
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 12:57:28 -0500

    comments

--- if_elif_else_statements.py
+++ if_elif_else_statements.py
@@ -1,12 +1,14 @@
+# This file demonstrates the use of if-elif-else statements 
 
 rainbow_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-if 'pink' in rainbow_colors:
+
+if 'pink' in rainbow_colors:            # This first test tests a value not in the list, so the test fails and it moves on to the next test
     print("Pink is a color of the rainbow!")
 
-elif 'red' in rainbow_colors:
+elif 'red' in rainbow_colors:           # This test passes, so the block of code that is after it will be executed
     print("Red is a color of the rainbow!")
 
-elif 'orange' in rainbow_colors:
+elif 'orange' in rainbow_colors:        # This test does not run because an if-elif-else statement only allows one test to pass and does not run the other tests
     print("Orange is a color of the rainbow!")
 
