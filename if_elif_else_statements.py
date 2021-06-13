c1d8600b124c25e7aed5544773ceede1c5ca6fb3
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 12:51:14 -0500

    init

---
+++ if_elif_else_statements.py
@@ -0,0 +1,12 @@
+
+rainbow_colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+if 'pink' in rainbow_colors:
+    print("Pink is a color of the rainbow!")
+
+elif 'red' in rainbow_colors:
+    print("Red is a color of the rainbow!")
+
+elif 'orange' in rainbow_colors:
+    print("Orange is a color of the rainbow!")
+
