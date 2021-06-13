167a38bda1c4e64dccce34b2e7bd94b1c5cff9c5
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 15:30:50 -0500

    worked on code

--- lists_and_if_and_for.py
+++ lists_and_if_and_for.py
@@ -1,14 +1,14 @@
 
 usernames = ['weeWilly123', 'Bryguy7', 'admin', 'Noobmaster69', 'SisterSky400']
 
-for name in usernames:
-    if usernames: 
+if usernames:
+    for name in usernames:
         if name == 'admin':
             print("Hello Admin! What would you like to do today? You have many options because you are the admin.")
 
         else:
             print(f"Hello {name}! What would you like to do?")
 
-    else: 
-        print("No users! We need to find some users!")
+else: 
+    print("No users! We need to find some users!")
         
