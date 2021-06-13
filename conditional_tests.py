d43e0d0601c893a550661500eaaedfeddc309de5
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-12 19:47:49 -0500

    added comments

--- conditional_tests.py
+++ conditional_tests.py
@@ -1,3 +1,11 @@
+
+# This file is my first practice with using conditional statements with lists. 
+# I started out with a list of characters from one of my favorite shows, and then I 
+# use conditional statements to determine if a character is in the list or not. 
+# If the character is not in the list, then it is added to the end of the list.
+# If the character is in the list, then it prints a message saying that is is already in the list.
+
+
 game_of_thrones_characters = ['jon snow', 'arya stark', 'sansa stark', 'tyrion lannister', 'the hound']
 
 new_character = 'khaleesi'
