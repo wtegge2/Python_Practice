4bf11dcb797d971b68c7017442dfd520413f2198
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2021-06-13 15:37:31 -0500

    added comments

--- lists_and_if_and_for.py
+++ lists_and_if_and_for.py
@@ -1,14 +1,17 @@
 
+# This file is an excercise in which I commbine lists with if statements and for loops. 
+
+# Initialized the list with some values
 usernames = ['weeWilly123', 'Bryguy7', 'admin', 'Noobmaster69', 'SisterSky400']
 
-if usernames:
-    for name in usernames:
-        if name == 'admin':
+if usernames:       # if statement at the beginning to test whether the list is empty or not
+    for name in usernames:      # if the list is not empty, then the program runs the for loop
+        if name == 'admin':         # looks for the value admin in the list and prints a special message when it finds it
             print("Hello Admin! What would you like to do today? You have many options because you are the admin.")
 
-        else:
+        else:               # prints message for every value but admin
             print(f"Hello {name}! What would you like to do?")
 
-else: 
+else:       # if the list is empty, prints message
     print("No users! We need to find some users!")
-        
+
